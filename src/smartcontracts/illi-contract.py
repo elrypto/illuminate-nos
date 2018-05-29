56a66f7edb13f7ceb9b12184da5a59306303c8be
lrypto <lloydrebello@gmail.com>
2018-05-29 09:08:45 -0400

    added test smart contract

---
+++ src/smartcontracts/illi-contract.py
@@ -0,0 +1,8 @@
+from boa.interop.Neo.Runtime import GetTrigger, CheckWitness, Notify, Log, GetTime, Serialize, Deserialize
+
+def Main():
+
+  print("ill-contract-run");
+  log("illi contract run");
+
+  return True
