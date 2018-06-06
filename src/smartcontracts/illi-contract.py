7a8aec57fa392b83b1540e8dff03c058b5d9280a
lrypto <lloydrebello@gmail.com>
2018-06-06 12:57:10 -0400

    fix the BOA list problem

--- src/smartcontracts/illi-contract.py
+++ src/smartcontracts/illi-contract.py
@@ -3,8 +3,4 @@ from boa.interop.Neo.Runtime import GetTrigger, CheckWitness
 def Main(caller):
     print("ill-contract-run");
 
-    isCaller = CheckWitness(caller)
-    if isCaller:
-        return True
-
-    return False
+    return True
