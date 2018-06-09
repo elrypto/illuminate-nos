8f7fa59bc64a8b14affcf082dd51552e333f035b
lrypto <lloydrebello@gmail.com>
2018-06-09 12:35:08 -0400

    stripped down to fix

--- src/smartcontracts/illi-contract.py
+++ src/smartcontracts/illi-contract.py
@@ -7,49 +7,6 @@ OWNER = b'\x03\x19\xe0)\xb9%\x85w\x90\xe4\x17\x85\xbe\x9c\xce\xc6\xca\xb1\x98\x9
 def Main():
     print("ill-contract-run")
 
-    trigger = GetTrigger()
+    ctx = GetContext()
 
-    if trigger == Verification():
-        print("Running Verification!")
-    #    is_owner = CheckWitness(OWNER)
-
-    #    if is_owner:
-    #        print("Is Owner!")
-#            return True
-
-#        print("Not Owner")
-#            return False
-
-    elsif trigger == Application():
-         print("Running Application!")
-
-    #      if not is_valid_addr(addr):
-#              print("Not Valid Address")
-              #return False
-
-          ctx = GetContext()
-
-          #if operation == 'add':
-        #      balance = Get(ctx, addr)
-        #      new_balance = balance + value
-    #          Put(ctx, addr, new_balance)
-    #          return new_balance
-
-    #      elif operation == 'remove':
-    #          balance = Get(ctx, addr)
-    #          Put(ctx, addr, balance - value)
-    #          return balance - value
-
-    #      elif operation == 'balance':
-    #          return Get(ctx, addr)
-
-    #      return False
-
-    #return False
-
-
-#def is_valid_addr(addr):
-
-  #if len(addr) == 20:
-    #  return True
-  #return False
+    return True
