075d145eb129564b4af9f8957e1bb9edd37f7084
lrypto <lloydrebello@gmail.com>
2018-06-11 13:48:23 -0400

    adding simple storage contract

--- src/smartcontracts/illi-contract.py
+++ src/smartcontracts/illi-contract.py
@@ -2,9 +2,6 @@ from boa.interop.Neo.Runtime import GetTrigger,CheckWitness
 from boa.interop.Neo.Storage import Get,Put,Delete,GetContext
 from boa.interop.Neo.TriggerType import Application, Verification
 
-OWNER = b'\x03\x19\xe0)\xb9%\x85w\x90\xe4\x17\x85\xbe\x9c\xce\xc6\xca\xb1\x98\x96'
-
-
 
 def Main(operation, addr, value):
     print("ill-contract-run")
