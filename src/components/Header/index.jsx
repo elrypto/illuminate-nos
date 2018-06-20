24748b32b6e187d72f4ee4d307c54f2651ecac5b
lrypto <lloydrebello@gmail.com>
2018-06-20 11:14:31 -0400

    updated nos api integrated

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,8 +5,6 @@ import { react } from "@nosplatform/api-functions";
 
 const { injectNOS, nosProps } = react.default;
 
-
-
 const styles = {
 };
 
@@ -22,7 +20,7 @@ class Header extends React.Component {
     }
   }
 
-/*
+
   componentDidMount(){
     var p = [];
     var p2 = [];
@@ -53,24 +51,34 @@ class Header extends React.Component {
 
   handleGetAddress = async () => await this.props.nos.getAddress();
   handleGetBalance = async scriptHash => await this.props.nos.getBalance(scriptHash);
-*/
+
 
   render() {
-    const { classes } = this.props;
+    const { classes, nos } = this.props;
 
-    // Get Balance
     const neo = "c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b";
-    // const gas = "602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7";
-    // const rpx = "ecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9";
+    const gas = "602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7";
+    const rpx = "ecc6b20d3ccac1ee9ef109af5a7cdb85706b1df9";
 
-    // (test) Invoke
-    const scriptHashNeoAuth = "2f228c37687d474d0a65d7d82d4ebf8a24a3fcbc";
-    const operation = "9937f74e-1edc-40ae-96ad-1120166eab1b";
-    const args = "ef68bcda-2892-491a-a7e6-9c4cb1a11732";
+    // Add your smart contract's scriptHash here
+    const scriptHash = "";
 
-    // Get Storage
-    const scriptHashNeoBlog = "85e9cc1f18fcebf9eb8211a128807e38d094542a";
-    const key = "post.latest";
+    // The operation of your smart contract you want to (test)invoke
+    const operation = "";
+
+    // The necessary arguments for you (test)invoke
+    const args = [];
+
+    // The storagekey you want to query
+    const key = "";
+
+    // The amount and recipient of your send function
+    const recipient = "";
+    const amount = "";
+
+    const invoke = { scriptHash, operation, args }; // and testInvoke
+    const getStorage = { scriptHash, key };
+    const send = { amount, asset: gas, recipient };
 
     return (
       <React.Fragment>
@@ -92,7 +100,7 @@ class Header extends React.Component {
                   <ul class="list-unstyled">
                     <li><a href="#" class="text-white">Profile</a></li>
                     <li><a href="#" class="text-white">Site History</a></li>
-                    <li class="text-white">NEO - [this.state.neoLastBalance] ([this.state.neoAddress.substring(0,12) + "..."])</li>
+                    <li class="text-white">NEO - {this.state.neoLastBalance} ({this.state.neoAddress.substring(0,12) + "..."})</li>
                     <li class="text-white">GAS - [this.state.gasLastBalance]</li>
                     <li class="text-white">Illi - [this.state.illiLastBalance]</li>
                   </ul>
